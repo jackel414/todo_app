47fbe6d497df3bd6d39e60bc05e7aaec6fa6e99c
Zachary Mays <zackmays@gmail.com>
2017-11-16 19:57:21 -0800

    Adds basic layout and initial Vue component

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -1,12 +1,39 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">To Do List</div>
 
-                    <div class="panel-body">
-                        I'm an example component!
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <input v-on:change="addTask($event.target.value)" type="text" class="form-control" placeholder="New task..." aria-label="New task...">
+                                <br />
+                                <p class="lead">This Week</p>
+                                <ul class="list-group">
+                                    <li v-show="tasks" v-for="task in tasks" class="list-group-item">
+                                        <span class="badge badge-pill badge-primary">{{ task.marker }}</span> {{ task.name }} <i v-on:click="addTaskToToday(task)" class="fa fa-arrow-right" aria-hidden="true"></i>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col">
+                                <p class="lead">Today</p>
+                                <ul class="list-group">
+                                    <li v-show="todaysTasks" v-for="task in todaysTasks" class="list-group-item">
+                                        <span class="badge badge-pill badge-primary">{{ task.marker }}</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                      </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">Weekly Ledger - Week of </div>
+
+                    <div class="card-body">
                     </div>
                 </div>
             </div>
@@ -16,8 +43,30 @@
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
+        data() {
+            return {
+                tasks: [
+                    {
+                        marker: '1',
+                        name: "Work on Site",
+                        recurring: false,
+                        done: false,
+                    }
+                ],
+                todaysTasks: []
+            }
+        },
+        methods: {
+            addTask: function (task) {
+                var newTask = {
+                    name: task
+                }
+
+                this.tasks.push(newTask);
+            },
+            addTaskToToday: function(task) {
+                this.todaysTasks.push(task);
+            }
         }
     }
 </script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,85 +11,20 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <script src="https://use.fontawesome.com/5c58b1cad5.js"></script>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+        <div id="app">
+            <div class="container">
+                <div>
+                    <p>&nbsp;</p>
+                    <example-component></example-component>
                 </div>
             </div>
         </div>
+        <script type="text/javascript" src="/js/app.js"></script>
     </body>
 </html>
